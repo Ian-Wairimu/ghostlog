9d065338e92649e8875706dd1d273201cecc0763
ian wairimu <wairimuianmoon@gmail.com>
2022-10-22 12:17:37 +0300

    created the create log drain and get token functions

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 
 const MONGODB_URI = process.env.DATABASE_URL
-
+const TOKEN_URL = process.env.VERCEL_URL
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
+const REDIRECT_URL = '';
 module.exports = {
-    MONGODB_URI
+    MONGODB_URI,
+    TOKEN_URL,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URL
 }
